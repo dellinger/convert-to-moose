7e848f6a5bc2d26a3a73650d0de27d93253ab460
David Ellinger <david.ellinger@me.com>
2015-12-21 18:42:45 -0800

    Added typescript test and gulp compilation task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+
+gulp.task('typescript', function () {
+    var tsProject = ts.createProject('tsconfig.json');
+    var tsResult = tsProject.src().pipe(ts(tsProject));
+    return tsResult.js.pipe(gulp.dest('dist'));
+});
