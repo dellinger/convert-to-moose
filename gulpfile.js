72f66c49e70d678091ef1c9cba072be3b5b8a948
David Ellinger <david.ellinger@me.com>
2016-01-20 21:23:35 -0800

    Removed a lot of the build tool process
    No need to have that right now if I'm just prototyping

--- gulpfile.js
+++ gulpfile.js
@@ -7,37 +7,11 @@ var runSequence = require('run-sequence');
 var clean = require('gulp-clean');
 var bower = require('gulp-bower');
 
-gulp.task('default',['build']);
-gulp.task('build', function(callback) {
-   runSequence(
-       'bower',
-       'typescript',
-       'inject-dependencies',
-       callback);
-});
+gulp.task('default',['typescript']);
+
 
 gulp.task('typescript', function () {
     var tsProject = ts.createProject('tsconfig.json');
     var tsResult = tsProject.src().pipe(ts(tsProject));
-    return tsResult.js.pipe(gulp.dest('public'));
+    return tsResult.js.pipe(gulp.dest('.'));
 });
-
-gulp.task('clean', function(){
-    return gulp.src('public/', {read:false})
-        .pipe(clean());
-});
-
-gulp.task('inject-dependencies', function(){
-    return gulp.src('index.tpl.html')
-        .pipe(inject(gulp.src(bower()),{name: 'bower'}))
-        .pipe(inject(
-            gulp.src('public/src/**/*.js', {read: false}),
-            {relative: true}
-        ))
-        .pipe(rename("index.html"))
-        .pipe(gulp.dest('public/'));
-});
-
-gulp.task('bower', function(){
-  return bower();
-})
